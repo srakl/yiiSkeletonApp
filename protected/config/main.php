f8ca59aafb1fc46e9d1c733f5d51f79a5090aec7
travisstroud <stroud.travis@gmail.com>
2013-05-01 21:33:08 -0600

    Update main.php

--- protected/config/main.php
+++ protected/config/main.php
@@ -38,7 +38,6 @@ return array(
             'urlFormat' => 'path',
             'showScriptName' => false,
             'rules' => array(
-                'login' => 'user/login',
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
